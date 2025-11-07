31f95fc948d278ab78d888732c2a659d30f985ff
tienvybui05 <tienvy27052005@gmail.com>
2025-11-07 18:27:18 +0700

    Commit tuan4

---
+++ Tuan4/Baitap2/app/src/main/java/com/example/uth/nav/Routes.kt
@@ -0,0 +1,13 @@
+package com.example.uth.nav
+
+object Routes {
+    const val comfirmpass = "comfirmpass"
+
+    const val verifycode = "verifycode"
+
+    const val forgerpass = "forgetpass"
+
+    const val createnewpass = "createnewpass"
+
+    const val detail = "detail"
+}
